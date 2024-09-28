99a46f3e65debe5ec1ab53c63b70c6736b8ae867
garylvov <67614381+garylvov@users.noreply.github.com>
2024-09-28 00:02:19 -0400

    Update update_gcc_patch.sh

--- setup_scripts/nvidia/update_gcc_patch.sh
+++ setup_scripts/nvidia/update_gcc_patch.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+# Use me like: sudo bash update_gcc_patch.sh --default_version 13
+
 # Function to display usage instructions
 usage() {
-    echo "Usage: $0 -default <version> <version_to_install> [<other_versions_to_install>...]"
-    echo "Example: $0 -default 13 11 12"
+    echo "Usage: $0 --default_version <version> [--bonus_versions <version_to_install>...]"
+    echo "Example: $0 --default_version 13 --bonus_versions 12 11"
     exit 1
 }
 
-# Check for minimum number of arguments
-if [ "$#" -lt 3 ]; then
+# Check if at least one argument (default_version) is provided
+if [ "$#" -lt 2 ]; then
     usage
 fi
 
-# Parse the -default argument and extract the default version
-if [ "$1" == "-default" ]; then
-    DEFAULT_VERSION=$2
-    shift 2
-else
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --default_version)
+            DEFAULT_VERSION="$2"
+            shift 2
+            ;;
+        --bonus_versions)
+            BONUS_VERSIONS=()
+            while [[ "$#" -gt 1 && ! "$2" =~ ^-- ]]; do
+                BONUS_VERSIONS+=("$2")
+                shift
+            done
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Check if default_version is set
+if [ -z "$DEFAULT_VERSION" ]; then
     usage
 fi
-
-# Remaining arguments are the versions to install
-VERSIONS_TO_INSTALL=("$@")
 
 # Update package lists and upgrade system
 echo "Updating package lists and upgrading system..."
@@ -38,17 +56,28 @@ echo "Adding Ubuntu Toolchain PPA..."
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -y
 
-# Install the specified GCC versions
-echo "Installing GCC versions: ${VERSIONS_TO_INSTALL[*]}..."
-for version in "${VERSIONS_TO_INSTALL[@]}"; do
-    sudo apt install gcc-$version g++-$version -y
-done
+# Install the default GCC version
+echo "Installing default GCC version: $DEFAULT_VERSION"
+sudo apt install gcc-$DEFAULT_VERSION g++-$DEFAULT_VERSION -y
+
+# Install bonus versions if provided
+if [ "${#BONUS_VERSIONS[@]}" -gt 0 ]; then
+    echo "Installing bonus GCC versions: ${BONUS_VERSIONS[*]}"
+    for version in "${BONUS_VERSIONS[@]}"; do
+        sudo apt install gcc-$version g++-$version -y
+    done
+fi
 
 # Set up alternatives for each installed GCC version
 echo "Setting up GCC alternatives..."
-for version in "${VERSIONS_TO_INSTALL[@]}"; do
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$version $version --slave /usr/bin/g++ g++ /usr/bin/g++-$version --slave /usr/bin/gcov gcov /usr/bin/gcov-$version
-done
+if [ "${#BONUS_VERSIONS[@]}" -gt 0 ]; then
+    for version in "${BONUS_VERSIONS[@]}"; do
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$version $version --slave /usr/bin/g++ g++ /usr/bin/g++-$version --slave /usr/bin/gcov gcov /usr/bin/gcov-$version
+    done
+fi
+
+# Set up alternatives for the default version
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$DEFAULT_VERSION $DEFAULT_VERSION --slave /usr/bin/g++ g++ /usr/bin/g++-$DEFAULT_VERSION --slave /usr/bin/gcov gcov /usr/bin/gcov-$DEFAULT_VERSION
 
 # Set the specified default GCC version
 echo "Setting GCC $DEFAULT_VERSION as the default..."
@@ -60,4 +89,3 @@ gcc --version
 g++ --version
 
 echo "GCC setup complete!"
-
